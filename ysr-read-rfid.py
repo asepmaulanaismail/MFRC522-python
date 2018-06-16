73b7f1baa31afdd29eaff3fa3d73960ce145bb8e
Asep Maulana Ismail <asepmaulanaismail@gmail.com>
2018-06-16 16:16:19 +0700

    bugs fix print

--- ysr-read-rfid.py
+++ ysr-read-rfid.py
@@ -7,7 +7,7 @@ continue_reading = True
 # Capture SIGINT for cleanup when the script is aborted
 def end_read(signal,frame):
     global continue_reading
-    print "Ctrl+C captured, ending read."
+    print("Ctrl+C captured, ending read.")
     continue_reading = False
     GPIO.cleanup()
     
@@ -15,12 +15,12 @@ def save(uid):
     try:
         r = requests.post('http://128.199.82.144:1506/absensi/tap', json={"uid": uid})
         if (r.status_code == 200):
-            print "SUCCESS"
+            print("SUCCESS")
         else:
-            print "FAILED"
-        print r.json()["message"]
+            print("FAILED")
+        print(r.json()["message"])
     except requests.exceptions.RequestException as e:
-        print e
+        print(e)
 
 # Hook the SIGINT
 signal.signal(signal.SIGINT, end_read)
@@ -29,8 +29,8 @@ signal.signal(signal.SIGINT, end_read)
 MIFAREReader = MFRC522.MFRC522()
 
 # Welcome message
-print "Welcome to the MFRC522 data read example"
-print "Press Ctrl-C to stop."
+print("Welcome to the MFRC522 data read example")
+print("Press Ctrl-C to stop.")
 
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
 while continue_reading:
@@ -50,7 +50,7 @@ while continue_reading:
 
         strUid = ("".join(str(uid[x]) for x in range(0, len(uid)-1))
         # Print UID
-        print "Card read UID: " + strUid)
+        print ("Card read UID: " + strUid)
         save(strUid)
     
         # This is the default key for authentication
@@ -67,5 +67,5 @@ while continue_reading:
             MIFAREReader.MFRC522_Read(8)
             MIFAREReader.MFRC522_StopCrypto1()
         else:
-            print "Authentication error"
+            print("Authentication error")
 
