b6cb26e5428b348d8ba66450f3b9ea167f67b4a5
Asep Maulana Ismail <asepmaulanaismail@gmail.com>
2018-06-17 10:49:20 +0700

    add pin rw

--- ysr-read-rfid.py
+++ ysr-read-rfid.py
@@ -19,16 +19,8 @@ def setup():
     GPIO.output(BeepPin, GPIO.HIGH)  # led on
     time.sleep(1)
     GPIO.output(BeepPin, GPIO.LOW) # led off
-    #lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33,31,29,32], numbering_mode=GPIO.BOARD)
-    #GPIO.setup(40, GPIO.OUT)
-    #GPIO.output(40, GPIO.HIGH)
-
-    #GPIO.setup(32, GPIO.OUT)
-    #GPIO.output(32, GPIO.HIGH)
-
-    #GPIO.setup(38, GPIO.OUT)
-    #GPIO.output(38, GPIO.LOW)
-    #lcd.write_string('Siap')
+    lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_rw=36, pin_e=35, pins_data=[33,31,29,32], numbering_mode=GPIO.BOARD)
+    lcd.write_string('Siap')
 
 
 def beep(mode):
